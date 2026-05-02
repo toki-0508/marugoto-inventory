88130be26d0f0cec662ba91eff525d0002b5ffba
zeno1192 <zeno1192@users.noreply.github.com>
2026-05-02 13:22:25 +0900

    Set GAS_URL to live web app endpoint

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 // Google Apps Script のウェブアプリ URL
-window.GAS_URL = '';
+window.GAS_URL = 'https://script.google.com/macros/s/AKfycbxVD_RS-2ucpH7s4lD8f1YAlGE0JjG0CzA5SHoiccoOQ3oo7XtWgbo81G0gR5_C_oQQ/exec';
 
 // バックエンドのスプレッドシート URL（管理画面の「📄」ボタンで開く）
 window.SHEET_URL = 'https://docs.google.com/spreadsheets/d/1Vwms7ByBkeVTKmKlsA-p0zvBP2cke_MqjpSGppoEjbU/edit?gid=0#gid=0';
