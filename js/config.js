a3358c5c3da1f22a8ae7c9e01ae2e59a173a823d
zeno1192 <zeno1192@users.noreply.github.com>
2026-05-02 13:10:31 +0900

    Update SHEET_URL with full anchor

--- js/config.js
+++ js/config.js
@@ -2,4 +2,4 @@
 window.GAS_URL = '';
 
 // バックエンドのスプレッドシート URL（管理画面の「📄」ボタンで開く）
-window.SHEET_URL = 'https://docs.google.com/spreadsheets/d/1Vwms7ByBkeVTKmKlsA-p0zvBP2cke_MqjpSGppoEjbU/edit';
+window.SHEET_URL = 'https://docs.google.com/spreadsheets/d/1Vwms7ByBkeVTKmKlsA-p0zvBP2cke_MqjpSGppoEjbU/edit?gid=0#gid=0';
